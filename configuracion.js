4e64854c3bda181863b8a411467fd16daded755a
Joshua Martinez <jmartinezb3@est.ups.edu.ec>
2022-03-10 18:24:18 -0500

    Cambios en server

--- configuracion.js
+++ configuracion.js
@@ -1,7 +1,6 @@
 const config = {
     // DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority',
     DB_URL: 'mongodb+srv://jmartinez:ups2022@clusterups.x8teh.mongodb.net/tiendita_jmartinez?retryWrites=true&w=majority',
-    PUERTO: 3000
+    PUERTO: process.env.port || 3000
 }
-
 module.exports = config
