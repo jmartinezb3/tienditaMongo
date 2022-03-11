28fc4a0c8d270161cb37ba45bac7abb4b84efbce
Joshua Martinez <jmartinezb3@est.ups.edu.ec>
2022-03-10 19:10:29 -0500

    Heroku CLI

--- configuracion.js
+++ configuracion.js
@@ -1,6 +1,6 @@
 const config = {
-    // DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority',
     DB_URL: 'mongodb+srv://jmartinez:ups2022@clusterups.x8teh.mongodb.net/tiendita_jmartinez?retryWrites=true&w=majority',
+    host: "joshuappups.herokuapp.com/",
     PUERTO: process.env.port || 3000
 }
 module.exports = config
